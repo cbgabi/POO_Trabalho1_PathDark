3d46a16549c5c64e7f7000f4620569ddd745b785
cbgabi <gabriellacbueno1@gmail.com>
2026-03-10 18:10:08 -0300

    Implement difficulty selection and refactor battle
    Refactor battle logic and add difficulty selection for the player.

--- main.py
+++ main.py
@@ -1,33 +1,61 @@
-from jogador import AnjoDemoniaco
-from inimigo import Sereia
+import random
+
+from jogador import Jogador
+from vocacao import Vocacao
+from inimigo import inimigos
 
 
-def batalha(jogador, inimigo):
-    while jogador.esta_vivo() and inimigo.esta_vivo():
+print(" RPG BATALHA ")
 
-        print(jogador.status())
-        print(inimigo.status())
+print("Escolha a dificuldade")
+print("1 - facil")
+print("2 - Médio")
+print("3 - dificil")
 
-        escolha = jogador.escolher_acao()
+opcao = int(input("Opção: "))
 
-        if escolha == "1":
-            print(jogador.atacar(inimigo))
-        elif escolha == "2":
-            print(jogador.habilidade_especial())
+if opcao == 1:
+    classe_inimigo = random.choice(inimigos["fracos"])
 
-        if inimigo.esta_vivo():
-            print(inimigo.atacar(jogador))
+elif opcao == 2:
+    classe_inimigo = random.choice(inimigos["medios"])
 
-        print()
+else:
+    classe_inimigo = random.choice(inimigos["fortes"])
 
-    if jogador.esta_vivo():
-        print("🏆 Jogador venceu!")
+inimigo = classe_inimigo()
+
+
+guerreiro = Vocacao("Guerreiro", 30, 10, 5, 2)
+
+nome = input("Nome do jogador: ")
+
+jogador = Jogador(nome, guerreiro)
+
+print(f"\n Um {inimigo.nome} apareceu!")
+
+turno = 1
+
+while jogador.vida > 0 and inimigo.vida > 0:
+
+    print(f"\nTurno {turno}")
+
+    if turno % 2 == 1:
+        jogador.atacar(inimigo)
     else:
-        print("💀 Inimigo venceu!")
+        inimigo.atacar(jogador)
+
+    jogador.status()
+    inimigo.status()
+
+    input("Pressione Enter...")
+    turno += 1
 
 
-if __name__ == "__main__":
-    anjo = AnjoDemoniaco("Malak")
-    sereia = Sereia("Sadna")
+print("\nFim da batalha!")
 
-    batalha(anjo, sereia)
+if jogador.vida > 0:
+    print("Você venceu!")
+    jogador.ganhar_exp(inimigo.exp_recompensa)
+else:
+    print("Você perdeu!")
