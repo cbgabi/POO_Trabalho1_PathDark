87dce3bb986beb84439cad1ad6e59d74102f1368
cbgabi <gabriellacbueno1@gmail.com>
2026-03-10 18:41:45 -0300

    Fix constructor method name in NPC class

--- npc.py
+++ npc.py
@@ -1,7 +1,8 @@
 class NPC:
-    def _init_(self, nome, fala):
+    def __init__(self, nome, fala):
         self.nome = nome
         self.fala = fala
 
     def falar(self):
+
         print(f"{self.nome} diz: {self.fala}")
