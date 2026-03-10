59857199eb2856c78a14e25a682f9dc8386eeb94
cbgabi <gabriellacbueno1@gmail.com>
2026-03-10 18:41:22 -0300

    Fix constructor method name in Vocacao class

--- vocacao.py
+++ vocacao.py
@@ -1,7 +1,8 @@
 class Vocacao:
-    def _init_(self, nome, bonus_vida, bonus_ataque, bonus_defesa, bonus_esquiva):
+    def __init__(self, nome, bonus_vida, bonus_ataque, bonus_defesa, bonus_esquiva):
         self.nome = nome
         self.bonus_vida = bonus_vida
         self.bonus_ataque = bonus_ataque
         self.bonus_defesa = bonus_defesa
+
         self.bonus_esquiva = bonus_esquiva
