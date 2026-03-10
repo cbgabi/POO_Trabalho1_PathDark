9d99e1ffa71963a69498aa388b43d0c8e1999b4f
cbgabi <gabriellacbueno1@gmail.com>
2026-03-10 18:10:59 -0300

    Implement experience gain and level up in Jogador
    Added experience and leveling system to Jogador class.

--- 1jogador.py
+++ 1jogador.py
@@ -1,15 +1,33 @@
-from entidade1 import Entidade
+from entidade import Entidade
 
 class Jogador(Entidade):
-    def _init_(self, nome, vocacao):
-        super()._init_(
+
+    def __init__(self, nome, vocacao):
+        super().__init__(
             nome,
             100 + vocacao.bonus_vida,
             10 + vocacao.bonus_defesa,
             10 + vocacao.bonus_ataque,
             5 + vocacao.bonus_esquiva
         )
+
         self.vocacao = vocacao
+        self.exp = 0
+        self.nivel = 1
 
     def mostrar_vocacao(self):
-        print(f" Vocação: {self.vocacao.nome}")
+        print(f"Vocação: {self.vocacao.nome}")
+
+    def ganhar_exp(self, quantidade):
+        print(f"\n Você ganhou {quantidade} EXP!")
+        self.exp += quantidade
+
+        if self.exp >= 100:
+            self.nivel += 1
+            self.exp = 0
+            self.vida += 20
+            self.ataque += 5
+            self.defesa += 3
+
+            print(" LEVEL UP!")
+            print(f"Você agora é nível {self.nivel}")
